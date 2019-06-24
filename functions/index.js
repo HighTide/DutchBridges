08f98db94fe4c6fe05b998dc4ef0470e5d6541b0
Jurre <IO@HighTide.dev>
2019-06-25 00:01:15 +0200

    Foreach loop broken

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 const {dialogflow} = require('actions-on-google');
+const tabletojson = require('tabletojson');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 //intents
 const BRIDGE_STATE = 'Bridge_State';
@@ -25,14 +28,14 @@ class Helper {
                     let bridgeState = obj[0]["open"];
 
                     if (bridgeState === "begaanbaar") {
-                        answer = ("De " + BridgeName + " brug is begaanbaar");
+                        answer = ("De " + BridgeName + " is begaanbaar");
                     } else //if (open == "gestremd")
                     {
-                        answer = ("De " + BridgeName + " brug is Open");
+                        answer = ("De " + BridgeName + " is Open");
                     }
                 } else {
                     console.log('3');
-                    answer = "De Spijkenisse brug geeft geen antwoord.";
+                    answer = "De brug geeft geen antwoord.";
                 }
                 this.conv.ask(answer);
                 resolve();
@@ -48,22 +51,46 @@ class Helper {
                     let obj = JSON.parse(body);
                     let BridgeName = obj[0]["id"];
                     let bridgeState = obj[0]["open"];
+                    bridgeState = "open";
                     let bridgeCloseTime = obj[0]["end"];
 
                     if (bridgeState === "begaanbaar") {
-                        answer = ("De " + BridgeName + " brug is niet open");
+                        answer = ("De " + BridgeName + " is niet open");
                     } else {
                         let dateNow = new Date();
                         let closeDate = new Date(bridgeCloseTime);
                         let closingTime = closeDate - dateNow;
-                        answer = ("De " + BridgeName + " brug gaat dicht in " + closingTime);
+                        this.checkCloseTime(BridgeName);
+                        answer = ("De " + BridgeName + " gaat dicht in " + closingTime);
                     }
                 } else {
                     console.log('3');
-                    answer = "De Spijkenisse brug geeft geen antwoord.";
+                    answer = "De brug geeft geen antwoord.";
                 }
                 this.conv.ask(answer);
                 resolve();
+            });
+        });
+    }
+
+    checkCloseTime(bridgeName) {
+        return new Promise((resolve, reject) => {
+            let answer = "";
+            request.get('https://www.brug-open.nl', (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const dom = new JSDOM(body);
+                    //console.log(body)
+                    const table = dom.window.document.querySelector("#wrapper > div.content-section-a > div.container > div > div:nth-child(1) > div > table").innerHTML;
+                    //console.log(table);
+                    let tableAsJson = tabletojson.convert("<table>"+table+"</table>");
+                    tableAsJson = tabletojson[0];
+
+                    tableAsJson.forEach(elem => console.log(elem))
+                }
+                else {
+                    console.log('3');
+                    answer = "De brug geeft geen antwoord.";
+                }
             });
         });
     }
