edbcdaee3a087a47b0166f11a6873c2234e7fc39
Jurre <IO@HighTide.dev>
2019-06-25 15:58:42 +0200

    I Promise you then.then.then.then

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,8 @@ const {JSDOM} = jsdom;
 const BRIDGE_STATE = 'Bridge_State';
 const BRIDGE_LOCATION = 'Bridge_Location';
 const BRIDGE_FUTURE = 'Bridge_Future';
+const BRIDGE_DURATION = 'Bridge_Duration';
+
 //variables for the HTTP requests
 const request = require('request');
 var headers = {};
@@ -37,8 +39,8 @@ class Helper {
                     console.log('3');
                     answer = "De brug geeft geen antwoord.";
                 }
-                this.conv.ask(answer);
-                resolve();
+                //this.conv.ask(answer);
+                resolve(answer);
             });
         });
     }
@@ -51,20 +53,20 @@ class Helper {
                     let obj = JSON.parse(body);
                     let BridgeName = obj[0]["id"];
                     let bridgeState = obj[0]["open"];
-                    bridgeState = "open";
+                    //bridgeState = "open";
 
                     if (bridgeState === "begaanbaar") {
                         answer = ("De " + BridgeName + " is niet open");
-                        this.conv.ask(answer);
-                        resolve();
+                        reject(answer);
                     } else {
-                        this.checkCloseTime(BridgeName);
+                        answer = BridgeName;
+                        resolve(answer);
                     }
                 } else {
                     console.log('3');
                     answer = "De brug geeft geen antwoord.";
-                    this.conv.ask(answer);
-                    resolve();
+                    reject(answer);
+
                 }
             });
         });
@@ -82,27 +84,70 @@ class Helper {
                     let tableAsJson = tabletojson.convert("<table>" + table + "</table>");
                     let tableAsJson1 = tableAsJson[0];
                     //console.log(tableAsJson1)
-                    bridgeName = 'Biesterbrug';
+                    //bridgeName = 'Vlotbrug';
                     tableAsJson1.forEach(function (entry) {
                         let name = entry['Naam / Plaats'];
                         if (name.includes(bridgeName)) {
                             console.log(entry['Naam / Plaats']);
                             console.log(entry['Duur']);
-                            let closingTime = ['Duur'];
+                            let closingTime = entry['Tot'];
 
                             console.log("Time Remaining:" + closingTime);
-                            answer = ("De " + bridgeName + " gaat dicht in " + closingTime);
+                            answer = ("De " + bridgeName + " gaat dicht om " + closingTime);
+                            resolve(answer);
                         }
                     });
                 } else {
                     console.log('3');
                     answer = "De brug geeft geen antwoord.";
+                    resolve(answer);
                 }
-                this.conv.ask(answer);
-                resolve();
+                console.log("resolver: closeBridge");
+                resolve(answer);
             });
         })
     };
+
+    checkOpenDuration(bridgeName) {
+        return new Promise((resolve, reject) => {
+            let answer = "";
+            request.get('https://www.brug-open.nl', (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    const dom = new JSDOM(body);
+                    //console.log(body)
+                    const table = dom.window.document.querySelector("#wrapper > div.content-section-a > div.container > div > div:nth-child(1) > div > table").innerHTML;
+                    //console.log(table);
+                    let tableAsJson = tabletojson.convert("<table>" + table + "</table>");
+                    let tableAsJson1 = tableAsJson[0];
+                    //console.log(tableAsJson1)
+                    //bridgeName = 'Vlotbrug';
+                    tableAsJson1.forEach(function (entry) {
+                        let name = entry['Naam / Plaats'];
+                        if (name.includes(bridgeName)) {
+                            console.log(entry['Naam / Plaats']);
+                            console.log(entry['Duur']);
+                            let closingTime = entry['Duur'];
+
+                            console.log("Time Remaining:" + closingTime);
+                            answer = ("De " + bridgeName + " is al " + closingTime + " Minuten Open");
+                            resolve(answer);
+                        }
+                    });
+                } else {
+                    console.log('3');
+                    answer = "De brug geeft geen antwoord.";
+                    resolve(answer);
+                }
+                console.log("resolver: closeBridge");
+                resolve(answer);
+            });
+        })
+    };
+
+    sayMessage(message) {
+        console.log(message);
+        this.conv.ask(message);
+    }
 }
 
 const app = dialogflow().middleware(conv => {
@@ -113,12 +158,17 @@ const app = dialogflow().middleware(conv => {
 //intents
 app.intent(BRIDGE_STATE, async (conv) => {
     const Bridge = conv.parameters[BRIDGE_LOCATION].toLowerCase();
-    await conv.helper.getBridge(Bridge);
+    await conv.helper.getBridge(Bridge).then(message => conv.helper.sayMessage(message), error => console.log(error));
 });
 
 app.intent(BRIDGE_FUTURE, async (conv) => {
     const Bridge = conv.parameters[BRIDGE_LOCATION].toLowerCase();
-    await conv.helper.closeBridge(Bridge);
+    await conv.helper.closeBridge(Bridge).then(bridge => conv.helper.checkCloseTime(bridge).then(message => conv.helper.sayMessage(message), error => console.log(error)), message => conv.helper.sayMessage(message));
+});
+
+app.intent(BRIDGE_DURATION, async (conv) => {
+    const Bridge = conv.parameters[BRIDGE_DURATION].toLowerCase();
+    await conv.helper.closeBridge(Bridge).then(bridge => conv.helper.checkOpenDuration(bridge).then(message => conv.helper.sayMessage(message), error => console.log(error)), message => conv.helper.sayMessage(message));
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
